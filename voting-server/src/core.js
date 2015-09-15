b8025ef5582019b618838befeaf9c745291834b5
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-09-15 15:16:57 -0700

    Initial State Spec

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -39,3 +39,5 @@ export function vote(state, entry){
     tally => tally + 1
   );
 }
+
+export const INITAL_STATE = Map();
--- voting-server/src/reducer.js
+++ voting-server/src/reducer.js
@@ -1,6 +1,6 @@
-import {setEntries, next, vote} from './core';
+import {setEntries, next, vote, INITAL_STATE} from './core';
 
-export default function reducer (state, action){
+export default function reducer(state = INITAL_STATE, action) {
   // figure out which function to call and call it
   switch (action.type) {
     case 'SET_ENTRIES' :
