efc9e41de91f5b3f1947b883785728addba51173
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-16 23:06:41 -0800

    Added some specs

--- voting-client/test/components/Results_spec.jsx
+++ voting-client/test/components/Results_spec.jsx
@@ -2,12 +2,12 @@ import React from 'react/addons';
 import {List, Map} from 'immutable';
 import Results from '../../src/components/Results';
 import {expect} from 'chai';
+import {describe, it} from 'mocha';
 
 const {renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate}
   = React.addons.TestUtils;
 
 describe('Results', () => {
-
   it('renders entries with vote counts or zero', () => {
     const pair = List.of('Trainspotting', '28 Days Later');
     const tally = Map({'Trainspotting': 5});
@@ -37,5 +37,16 @@ describe('Results', () => {
     Simulate.click(React.findDOMNode(component.refs.next));
 
     expect(nextInvoked).to.equal(true);
+  });
+
+  it('renders the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Results winner="Trainspotting"
+               pair={["Trainspotting", "28 Days Later"]}
+               tally={Map()} />
+    );
+    const winner = React.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
   });
 });
