e39ed8e11b71e0c22d63c1d0ffe1d51bc02e070c
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-20 16:40:34 -0800

    using redux on the client for results and voting!

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -1,13 +1,13 @@
 import React from 'react/addons';
 import {List} from 'immutable';
-import Voting from '../../src/components/Voting';
+import {Voting} from '../../src/components/Voting';
 import {expect} from 'chai';
+import {describe, it } from 'mocha';
 
 const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate}
   = React.addons.TestUtils;
 
 describe('Voting', () => {
-
   it('renders a pair of buttons', () => {
     const component = renderIntoDocument(
       <Voting pair={["Trainspotting", "28 Days Later"]} />
