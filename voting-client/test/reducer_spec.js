a5d12401a886645aea0afdb8572003f643094e6e
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-22 23:57:13 -0800

    added tests for hasVoted

--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -67,8 +67,8 @@ describe('reducer', () => {
     const state = fromJS({
       vote: {
         pair: ['Trainspotting', '28 Days Later'],
-        tally: {Trainspotting: 1}
-      }
+        tally: {Trainspotting: 1},
+      },
     });
     const action = {type: 'VOTE', entry: 'Trainspotting'};
     const nextState = reducer(state, action);
@@ -95,6 +95,30 @@ describe('reducer', () => {
       vote: {
         pair: ['Trainspotting', '28 Days Later'],
         tally: {Trainspotting: 1},
+      },
+    }));
+  });
+  it('removes hasVoted on SET_STATE if pair changes', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1},
+      },
+      hasVoted: 'Trainspotting',
+    });
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Sunshine', 'Slumdog Millionaire'],
+        },
+      },
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Sunshine', 'Slumdog Millionaire'],
       },
     }));
   });
