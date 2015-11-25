b2cdbf5080cdf47d00d9a6309d67caa4ce943753
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-24 22:42:54 -0800

    Action creators wired up to voting

--- voting-client/src/remote_action_middleware.js
+++ voting-client/src/remote_action_middleware.js
@@ -1,4 +1,7 @@
-export default store => next => action => {
+export default socket => store => next => action => {
   console.log('in middleware', action);
+  if (action.meta && action.meta.remote) {
+    socket.emit('action', action);
+  }
   return next(action);
 };
